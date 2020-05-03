e66b048a221ca2fc17c03944fd29e8af77915a6a
NgugiMuthoni90210 <ngugimuthoni43@gmail.com>
2020-05-03 13:21:49 +0300

    --- Auto-Git Commit ---

--- app/main/views.py
+++ app/main/views.py
@@ -4,7 +4,7 @@ from ..models import User,Pitch,Comment
 from .. import db,photos
 from .forms import UpdateProfile,PitchForm,CommentForm
 from flask_login import login_required,current_user
-from datetime import datetime
+import datetime
 
 # Views
 @main.route('/')
@@ -108,7 +108,7 @@ def promotion_pitches():
 @main.route('/pitch/<int:id>', methods = ['GET','POST'])
 def pitch(id):
     pitch = Pitch.get_pitch(id)
-    posted_date = pitch.posted.datetime.datetime.strptime ('%a, %d %b %Y')
+    # posted_date = pitch.posted.strftime('%b %d, %Y')
 
     if request.args.get("like"):
         pitch.likes = pitch.likes + 1
@@ -137,7 +137,7 @@ def pitch(id):
 
     comments = Comment.get_comments(pitch)
 
-    return render_template("pitch.html", pitch = pitch, comment_form = comment_form, comments = comments, date = posted_date)
+    return render_template("pitch.html", pitch = pitch, comment_form = comment_form, comments = comments)
 
 @main.route('/user/<uname>/pitches')
 def user_pitches(uname):
